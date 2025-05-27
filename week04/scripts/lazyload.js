a9ad977bcab97f0d85594d17a0dd9a18c4589e54
thk3306 <rodingemil@gmail.com>
2025-05-27 15:05:52 +1200

    Created laztload page, and filtered temples website page

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,30 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+// Get the current date
+const today = new Date();
+
+// Select the element with the id "year"
+const yearElement = document.querySelector("#year");
+
+// Set the innerHTML of the selected element to the current year
+if (yearElement) {
+    yearElement.innerHTML = today.getFullYear();
+}
+
+let lastModified = document.lastModified;
+// Select the element with the id "lastModified"
+const lastModifiedElement = document.querySelector("#lastModified");
+
+// Set the innerHTML of the selected element to the last modified date
+if (lastModifiedElement) {
+    lastModifiedElement.innerHTML = lastModified;
+}
+
+
+
